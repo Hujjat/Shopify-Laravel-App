38e6f33dccc9554a2e5a7f867d0939fc468175f3
Hujjat Nazari <Hujjat@users.noreply.github.com>
2020-06-26 21:08:34 +1000

    17 - Wishlist Database Structure in Laravel Shopify App

---
+++ database/migrations/2020_06_26_110326_create_wishlists_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWishlistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wishlists', function (Blueprint $table) {
+            $table->id();
+            $table->string('shop_id');
+            $table->string('customer_id');
+            $table->string('product_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wishlists');
+    }
+}
